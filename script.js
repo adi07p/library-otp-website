721d41da1977ec3d8899526f55c7dbf980eed83c
adiopdev <172579501+adiopdev@users.noreply.github.com>
2024-08-14 11:38:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+// JavaScript code to enforce the white page and message
+document.body.style.backgroundColor = "white";
+document.body.innerHTML = '<div class="message">Onboarding is currently paused</div>';
